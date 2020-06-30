469261f5fc8b36f75445a2b974d908963b5195f5
w.benarto <w.benarto@gmail.com>
2020-06-30 11:19:47 -0700

    Java array manipulation

--- bigO.java
+++ bigO.java
@@ -74,3 +74,103 @@ array_2d = [[1,2,3],[4,2,7],[2,3,3]]
 T3 = O(1) + n^2*O(1) + O(1)
 T3 = O(n^2)
 quadratic time complexity
+
+
+
+i := 0
+while i < n 
+    j = 0
+    while j < 3*n 
+        j = j + 1
+    j = 0
+    while j < 2*n 
+        j = j + 1
+    i = i + 1
+
+// starting from zero, loop until n excluding
+// inside loop, 3*n increment j
+// another inside loop, 2*n  increment j
+
+bigO 
+f(n) = n * (3n + 2n) = 5n^^2
+O(n^^2)
+
+
+
+i:= 0
+while i < 3*n
+    j:= 10
+    while j <= 50 
+        j = j + 1
+    j = 0
+    while j< n*n*n
+        j = j + 2
+    i=i+1
+
+// outside loop, starting zero to 3*n
+// inside loop, starting from 10 to 50, increment j by 1
+// another inside loop, from 0 to cubic n, increment j by 2
+
+f(n)= 3n *(40 + N^^3/2)
+O(n) = 120n + 9n^^4/2
+O(n) = n^^4
+
+The most dominant term in f(n)
+and get rid of the coefficient
+
+
+
+// Example Code
+@SuppressWarnings("unchecked")
+// array class generic of type 
+public class Array <T> implements Iterable <T> {
+
+    private T [] arr;
+    private int len = 0;
+    private int capacity = 0;
+
+    // constructor
+    public Array() {this(16);}
+
+    public Array(int capacity) {
+        if(capacity<0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+
+        public int size() { return len; }
+        public boolean isEmpty() { return size() === 0; }
+
+        public T get(int index) { return arr[index]; }
+        public void set(int index, T elem) {
+            arr[index] = elem;
+        }
+
+        public void clear() {
+            for (int i=0; i<capacity; i++)
+                arr[i] = null;
+            len = 0;
+         }
+        
+        // add method
+        public void add(T elem) {
+
+            if (len+1 >= capacity) {
+                if(capacity == 0) capacity = 1;
+                else capacity *= 2;
+                T[] new_arr = (T[]) new Object[capacity];
+                for(int i=0; i< len; i++)
+                    new_arr[i] = arr[i];
+            }  
+            arr[len++] = elem;
+        }
+
+        public boolean remove(Object obj) {
+            for (int i = 0; i < len; i++) {
+                if (arr[i].equals(obj)){
+                    removeAt(i); return true; 
+                }
+            }
+            return false
+        }
+    }
+}
