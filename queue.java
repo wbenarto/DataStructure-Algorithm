6a3410b241d751c390baaa0aa6a83c17d7c40e19
w.benarto <w.benarto@gmail.com>
2020-07-01 12:00:44 -0700

    queue

---
+++ queue.java
@@ -0,0 +1,43 @@
+public class Queue <T> implements Iterable <T> {
+
+    // Java implementation of doubly linked list
+    private java.util.LinkedList <T> list = new java.util.LinkedList <T> ();
+
+    // constructor to create empty queue
+    public Queue() { }
+    // constructor optional empty queue with first elem
+    public Queue(T firstElem) { 
+        offer(firstElem);
+    }
+
+    // Return the size of the queue
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0
+    }
+
+    public T peek() {
+
+        // throw an error if list is empty, if not see if list got first elem
+        if (isEmpty()) 
+            throw new RuntimeException('Queue Empty');
+        return list.peekFirst()
+    }
+
+    public T poll() {
+        if(isEmpty())
+            throw new RuntimeException("Queue Empty");
+        return list.removeFirst();
+    }
+
+    public void offer(T elem) {
+        list.addLast(elem);
+    }
+
+    @Override public java.util.Iterator <T> iterator () {
+        return list.iterator();
+    }
+}
---
+++ stack.js
@@ -0,0 +1,28 @@
+public class Stack <T> implements Iterable <T> {
+    
+    private java.util.LinkedList <T> list = new java.util.LinkedList <T>();
+
+    public Stack() { }
+
+    public Stack (T firstElem) {
+        PushManager(firstElem);
+    }
+
+    public int size() {
+        return this.list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(T elem) {
+        list.addLast(elem);
+    }
+
+    public T pop() {
+        if (this.isEmpty())
+        throw new this.java.util.EmptyStackException();
+    }
+
+}
