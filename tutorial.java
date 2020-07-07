568a08f28d0b572364510cb2dceee10e2a1355ce
w.benarto <w.benarto@gmail.com>
2020-07-07 11:34:13 -0700

    creating objects

--- tutorial.java
+++ tutorial.java
@@ -9,8 +9,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // to print out in console
-        System.out.println("Hello World!");
+		System.out.println("hello world!");
+		System.out.println("Hellow !");
+		
+		// primitive data type int, double, boolean, char = not changeable
+		// create a variable with integer as data type
+		int helloWorld = 5;
+		
+		// any number that has floating decimal point
+		double num2 = 5.0;
+		
+		// true or false 
+		boolean b = true;
+		
+		// any character. different than string. just one character
+		char c = 't';
+		
+		// Not primitive data type
+		// String can contain anything, have to use "". 
+		String str = "tim";
+		
+		// assigning variable to another variable
+		int tim = helloWorld;
+		
+		
+		System.out.println(helloWorld*2/4+2);
 	}
 
 }
