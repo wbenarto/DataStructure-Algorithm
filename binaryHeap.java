0c5fe8b5429a42545678035f3b618de0c98c8519
w.benarto <w.benarto@gmail.com>
2020-07-07 11:10:15 -0700

    java tutorial

--- binaryHeap.java
+++ binaryHeap.java
@@ -32,3 +32,70 @@ remove(12)
 remove(3)
 poll()
 remove(6)
+
+import java.util.*;
+
+class PQueue <T extends Comparable<T>> {
+    // number currently in the heap
+    private int heapsize = 0;
+
+    // internal capacity
+    private int heapCapacity = 0;
+
+    // a dynamic list to track the elements inside heap  
+    private List <T> heap = null;
+
+    private Map <T, TreeSet<Integer> map = new HashMap<>();
+
+    // construct a priority 
+    public PQueue() {
+        this(1);
+    }
+
+    // construct a priority queue with an initial capacity
+    public PQueue(int sz) {
+        heap = new ArrayList<>(sz);
+    }
+
+    public PQueue (T[] elems) {
+        heapsize = heapCapacity = elems.length;
+        heap = new ArrayList<T>( heapCapacity );
+
+        for(int i = 0; i < heapSize; i++) {
+            mapAdd(elems[i], i);
+            heap.add(elems[i]);
+        }
+
+        for (int i = Math.max(0, (heapSize/2)-1); i>=0; i--)
+            sink(i);
+    }
+
+    public Pqueue (Collection <T> elems) {
+        this(elems.size());
+        for( T elem : elems) add(elem);
+    }
+
+    // returns true/false depending on if PR is empty
+    public boolean isEmpty() {
+        return heapSize == 0;
+    }
+
+    public void clear() {
+        for (int i=0; i < heapCapacity; i++)
+            heap.set(i, null);
+        heapSize = 0;
+        map.clear();
+    }
+
+    public T peek() {
+        if (isEmpty()) return null;
+        return heap.get(0);
+    }
+
+    public T poll() {
+        return removeAt(0);
+    }
+
+    
+
+}
---
+++ tutorial.java
@@ -0,0 +1,23 @@
+// Object Oriented Programming
+// Classes has special methods 
+
+// Create new project, create package, create new class
+
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // to print out in console
+        System.out.println("Hello World!");
+	}
+
+}
+
+
+
+
+
+
+// System.out.println(""); // to print in console 
