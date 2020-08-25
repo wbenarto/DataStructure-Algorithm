477cf8fe95447e267f5ae422b9e9da0f6ef9277f
w.benarto <w.benarto@gmail.com>
2020-08-25 12:54:18 -0700

    chaining hash table

---
+++ hash-Chaining.js
@@ -0,0 +1,70 @@
+
+class HashTable {
+    constructor() {
+        this.size=1000;
+        // need the bucket to store multiple values
+        // creating nested array inside of bucket
+        this.buckets = Array(1000).fill(null).map(() => []);
+    }   
+
+    hash(key) {
+        let hash = 0;
+
+        for (const char of key) {
+            hash += char.charCodeAt(0)
+        }
+
+        return hash%this.size
+    }
+
+    // set value to a specific key
+    set(key, val) {
+        const hashKey = this.hash(key)
+        // nested array, for the given key hash
+        const bucketArray = this.buckets[hashKey] 
+        // check if nested array consisted a value in that key
+        const storedElement = bucketArray.find((el) => {return el.key === key})
+
+        // if we found something
+        if (storedElement) {
+            storedElement.val = val
+        } else {
+            bucketArray.push({key: key, val: val})
+        }
+        console.log(this.buckets)
+    }
+
+    get(key) {
+        const hashKey = this.hash(key)
+        return this.buckets[hashKey]
+    }
+
+    showInfo(key) {
+        for (const key in this.buckets) {
+            if(this.buckets[key] !== null) {
+                console.log(key, this.buckets[key])
+            }
+        }
+    }
+
+
+}
+
+function findFirst(str) {
+    
+    const table = new HashTable();
+    
+        for (const char of str) {
+            console.log('char :' + char)
+            console.log('table[char]' + table[char])
+            if (table[char]) {
+                console.log('table[char] :' + table[char] )
+                console.log('result: ' + char)
+                return char;
+            }
+            table[char] = 1;
+        }
+    }
+
+findFirst("ball")
+findFirst("dummy")
--- hash-table.js
+++ hash-table.js
@@ -13,12 +13,15 @@
 // }
 // time complexity of O(n^2);
 
-
+// const word = "Basketball"
 // function findFirst(str) {
 //     const table = {};
 
 //     for (const char of str) {
+//         console.log('char :' + char)
+//         console.log('table[char]' + table[char])
 //         if (table[char]) {
+//             console.log('table[char] :' + table[char] )
 //             return char;
 //         }
 //         table[char] = 1;
@@ -30,62 +33,126 @@
 // console.log(findFirst(word))
 
 
-class HashTable {
-    constructor () {
-        this.size = 1000;
-        //creating buckets consisting array for 100 empty slots
-        this.buckets = Array(1000).fill(null).map(()=>[]);
-    }
+// class HashTable {
+//     constructor () {
+//         this.size = 1000;
+//         //creating buckets consisting array for 100 empty slots
+//         this.buckets = Array(1000).fill(null).map(()=>[]);
+//     }
 
-    // Hash Method
+//     // Hash Method
+//     hash(key) {
+//         let hash = 0;
+//         for (const char of key) {
+//             // built in method to call in string
+//             hash += char.charCodeAt(0)
+//         }
+//         return hash%this.size;
+//     }
+
+//     // set key to a value
+//     set(key, value) {
+//         const keyHash = this.hash(key);
+//         const bucketArray = this.buckets[keyHash];
+//         const storedElement = bucketArray.find((el) => {
+//             return el.key === key;
+//         });
+
+//         if(storedElement) {
+//             storedElement.val = value;
+//         } else {
+//             bucketArray.push({ key: key, val: value})
+//         }
+
+//     }
+
+//     get(key) {
+//         const keyHash = this.hash(key);
+//         const bucketArray = this.buckets[keyHash];
+//         const storedElement = bucketArray.find(el => {
+//             return el.key === key;
+//         })
+//         return storedElement;
+//     }
+
+//     showInfo() {
+
+//         // use for in loop to get keys
+//         for( const key in this.buckets) {
+//             if( this.buckets[key] !== null) {
+//                 console.log(key, this.buckets[key]);
+//             }
+//         }
+//     }
+  
+// }
+
+
+// const table1 = new HashTable();
+
+
+
+class HashTable {
+    constructor() {
+        this.size=1000;
+        this.buckets = Array(1000).fill(null);
+    }   
+
     hash(key) {
         let hash = 0;
+
         for (const char of key) {
-            // built in method to call in string
             hash += char.charCodeAt(0)
         }
-        return hash%this.size;
+
+        return hash%this.size
     }
 
-    // set key to a value
-    set(key, value) {
-        const keyHash = this.hash(key);
-        const bucketArray = this.buckets[keyHash];
-        const storedElement = bucketArray.find((el) => {
-            return el.key === key;
-        });
+    // set value to a specific key
+    set(key, val) {
+        const hashKey = this.hash(key)
+        const bucketArray = this.buckets[hashKey] 
+        const storedElement = bucketArray.find((el) => {return el.key === key})
 
-        if(storedElement) {
-            storedElement.val = value;
+        if (storedElement) {
+            storedElement.val = val
         } else {
-            bucketArray.push({ key: key, val: value})
+            bucketArray.push({key: key, val: val})
         }
-
+        console.log(this.buckets)
     }
 
     get(key) {
-        const keyHash = this.hash(key);
-        const bucketArray = this.buckets[keyHash];
-        const storedElement = bucketArray.find(el => {
-            return el.key === key;
-        })
-        return storedElement;
+        const hashKey = this.hash(key)
+        return this.buckets[hashKey]
     }
 
-    showInfo() {
-
-        // use for in loop to get keys
-        for( const key in this.buckets) {
-            if( this.buckets[key] !== null) {
-                console.log(key, this.buckets[key]);
+    showInfo(key) {
+        for (const key in this.buckets) {
+            if(this.buckets[key] !== null) {
+                console.log(key, this.buckets[key])
             }
         }
     }
-  
+
+
 }
 
+function findFirst(str) {
+    
+    const table = new HashTable();
+    
+        for (const char of str) {
+            console.log('char :' + char)
+            console.log('table[char]' + table[char])
+            if (table[char]) {
+                console.log('table[char] :' + table[char] )
+                console.log('result: ' + char)
+                return char;
+            }
+            table[char] = 1;
+        }
+    }
 
-const table1 = new HashTable();
-
-
-
+findFirst("ball")
+findFirst("dummy")
