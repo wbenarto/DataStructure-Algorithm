e1656c8f0ce4ffedc4a00274b642ca4ecce75348
w.benarto <w.benarto@gmail.com>
2020-08-27 14:42:20 -0700

    add path into tree

--- App.js
+++ App.js
@@ -1,33 +1,95 @@
-// Rebuild stack in linked list
+// // Rebuild stack in linked list
 
-import { linkedList } from './linkedList.js';
+// import { linkedList } from './linkedList.js';
 
-class Stack {
-    constructor() {
-        this.list = new linkedList();
+// class Stack {
+//     constructor() {
+//         this.list = new linkedList();
+//     }
+
+//     push(value) {
+//         this.list.prepend(value);
+//     }
+
+//     pop() {
+//         return this.list.deleteHead();
+//     }
+
+//     isEmpty() {
+//         return !this.list.head;
+//     }
+
+//     toArray() {
+//         return this.list.toArray();
+//     }
+//  }
+
+//  const stack = new Stack();
+
+//  stack.push('Code');
+//  stack.push('Push Up');
+//  stack.push('Sit up');
+//  console.log(stack.toArray())
+//  console.log('linkedliststack fired')
+
+// class Node {
+//     constructor(value, parentNode = null) {
+//         this.children = [];
+//         this.parent = parentNode;
+//         this.value = value;
+//     }
+
+//     addNode(value) {
+//         const node = new Node(value, this)
+//         // creating new node then pushed to children array
+//         this.children.push(node);
+//         return {node: node, index: this.children.length-1}
+//     }
+//     //removing one node
+//     remove(index) {
+//         this.children.splice(index,1)
+//     }
+// }
+
+// class Tree {
+//     constructor(rootValue) {
+//         this.root = new Node(rootValue);
+//     }
+// }
+
+
+class Node {
+    constructor(val, parentNode=null) {
+        this.children = [];
+        this.parent = parentNode;
+        this.val = val
     }
 
-    push(value) {
-        this.list.prepend(value);
+    addNode(value) {
+        const segment = value.split('/');
+        const existingChildNode = this.children.find(child=>{
+
+        })
+        if (segments === 0) {
+            return;
+        }
+
+        if (segments === 1) {}
+
+        this.children.push(segment)
+        console.log(this.children)
     }
 
-    pop() {
-        return this.list.deleteHead();
+    removeNode() {
+
     }
+}
 
-    isEmpty() {
-        return !this.list.head;
+class Tree {
+    constructor(rootValue) {
+        this.root = new Node(rootValue)
     }
+}
 
-    toArray() {
-        return this.list.toArray();
-    }
- }
-
- const stack = new Stack();
-
- stack.push('Code');
- stack.push('Push Up');
- stack.push('Sit up');
- console.log(stack.toArray())
- console.log('linkedliststack fired')
+const newTree = new Tree();
+newTree.root.addNode('1')
--- tree.js
+++ tree.js
@@ -32,12 +32,35 @@ class Node {
     }
 
     addNode(value) {
-        const node = new Node(value, this)
-        this.children.push(value);
-        return {node: node,index: this.children.length -1}
+        const segments = value.split('/');
+
+        // check if segments is empty, return nothing
+        if(segments.length === 0) {
+            return;
+        }
+        //
+        if(segments.length === 1) {
+            const node = new Node(segments[0], this)
+            this.children.push(node);
+            return {node: node,index: this.children.length -1}
+        }
+        // check if childen value equals to first element in segments
+        const existingChildNode= this.children.find(child=>child.value === segments[0])
+
+        // if existingNode is true, call that node and pass in the remaining segments
+        if(existingChildNode) {
+            existingChildNode.addNode(segments.slice(1).join("/"))
+        } else {
+            const node = new Node(segments[0], this) ;
+            node.addNode(segments.slice(1).join('/'));
+            this.children.push(node);
+            return {node: node, index: this.children.length-1};
+        }
+        
+       
     }
 
-    remove(index) {
+    removeNode(index) {
         return this.children.splice(index, 1)
     }
 }
@@ -46,12 +69,26 @@ class Tree {
     constructor(rootValue) {
         this.root = new Node(rootValue)
     }
+
+    add(path) {
+        this.root.addNode(path)
+    }
+
+    remove(path) {
+
+    }
 }
-const fileSystem = new Tree('/');
-const gamesNode = fileSystem.root.addNode('/games');
-const documentsNode = fileSystem.root.addNode('/documents')
+const filesystem = new Tree('/');
 
-gamesNode.node.addNode('cod.exe');
-documentsNode.node.addNode('results.txt');
+// ------ using addNodes to create contents ----------
+// const gamesNode = fileSystem.root.addNode('/games');
+// const documentsNode = fileSystem.root.addNode('/documents')
 
-console.log(fileSystem)
+// gamesNode.node.addNode('cod.exe');
+// documentsNode.node.addNode('results.txt');
+
+filesystem.add('/documents/personal/tax.docx');
+filesystem.add('/games/cod.exe');
+
+
+console.log(filesystem)
