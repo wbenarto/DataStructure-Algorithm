ceb09841fe259f8de166e482678ad104f7b82006
w.benarto <w.benarto@gmail.com>
2020-09-08 14:34:35 -0700

    remove node

--- App.js
+++ App.js
@@ -1,117 +1,71 @@
 class Node {
-    constructor (value, parentNode = null) {
-        this.children = [];
-        this.parent = parentNode;
+    constructor(value, parentNode = null) {
+        this.parent = null;
+        this.right = null;
+        this.left = null;
         this.value = value;
     }
 
-    addNode (value) {
-        const segments = value.split('/');
-
-        if (segments.length === 0) {
+    add (value) {
+        if (this.value === null) {
+            this.value = value;
             return;
         }
 
-        if (segments.length === 1) {
-            const node = new Node(segments[0], this);
-            this.children.push(node);
-            return {node: node, index: this.children.length -1};
-        }
+        // value to be added larger than this.value, so put it on the right 
+        if (this.value < value) {
+            if (this.right) {
+                this.right.add(value);
+                return;
+            }
 
-        const existingChildNode = this.children.find(child=>child.value === segments[0]);
-        if(existingChildNode) {
-            existingChildNode.addNode(segments.slice(1).join('/'))
-        } else {
-            const node = new Node(segments[0], this);
-            node.addNode(segments.slice(1).join('/'));
-            this.children.push(node);
-            return {node: node, index: this.children.length -1}
-        }
-    }
-
-    removeNode (value) {
-        const segments = value.split('/');
-
-        if (segments.length === 0) {
+            const newNode = new Node(value);
+            this.right = newNode;
             return;
         }
 
-        if (segments.length === 1) {
-            const existingNodeIndex = this.children.findIndex(child => child.value === segments[0])
-            if (existingNodeIndex < 0) {
-                throw new Error('not here')
+        // value to be added smaller than this.value, put it on the left
+        if (this.value > value) {
+            if (this.left) {
+                this.left.add(value);
+                return;
             }
-            this.children.splice(existingNodeIndex, 1)
-        }
-
-        if (segments.length > 1) {
-            const existingChildNode = this.children.find(child=>child.value === segments[0])
-            if (!existingChildNode) {
-                throw new Error('not here')
-            }
-            existingChildNode.removeNode(segments.slice(1).join('/'))
+            const newNode = new Node(value);
+            this.left = newNode;
+            return;
         }
     }
 
     find (value) {
-        console.log(this);
+        if (this.value === value ) {
+            return this;
+        }
 
-        for (const child of this.children) {
-            if (child.value === value) {
-                return child;
-            }
+        // check if bigger or smaller, if smaller look left, if bigger look right
+        if (this.value < value && this.right) {
+          return  this.right.find(value);
+        }
 
-            const nestedChildNode = child.find(value);
-            if (nestedChildNode) {
-                return nestedChildNode;
-            }
+        if (this.value > value ** this.left) {
+            return this.left.find(value);
         }
     }
-
 }
 
 class Tree {
-    constructor (rootValue) {
-        this.root = new Node(rootValue)
+    constructor () {
+        this.root = null;
     }
 
-    add (path) {
-        this.root.addNode(path);
+    add (value) {
+        this.root.add(value);
     }
 
     remove (value) {
-        this.root.removeNode(path);
+        this.root.remove(value);
     }
 
     find (value) {
-        if (this.root.value === value) {
-            return this.root
-        }
-        return this.root.find(value)
+        this.root.find(value)
     }
 }
-
-const filesystem = new Tree('/');
-
-// // ------ using addNodes to create contents ----------
-// // const gamesNode = fileSystem.root.addNode('/games');
-// // const documentsNode = fileSystem.root.addNode('/documents')
-
-// // gamesNode.node.addNode('cod.exe');
-// // documentsNode.node.addNode('results.txt');
-
-filesystem.add('documents/personal/tax.docx');
-filesystem.add('documents/2ndchild')
-filesystem.add('games/cod.exe');
-filesystem.add('work')
-filesystem.add('work/code/apply')
-filesystem.add('work/lookforthis')
-filesystem.add('play')
-
-// remove the last which is cod.exe
-// filesystem.remove('games/cod.exe')
-
-// finding node with this value
-console.log(filesystem.find('2ndchild'))
-
-console.log(filesystem)
--- binarySearchTree.js
+++ binarySearchTree.js
@@ -3,28 +3,73 @@ class Node {
         this.value = value;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 
     add(value) {
         if (this.value === null) {
             this.value = value;
+            return;
         }
-        return;
+        
 
         // check if value is smaller or bigger
         if (this.value < value) {
+            if (this.right) {
+                this.right.add(value);
+                return;
+            }
             const newNode = new Node(value);
+            newNode.parent = this;
             this.right = newNode;
             return;
         }
 
         if (this.value > value) {
+            if(this.left) {
+                this.left.add(value);
+                return;
+            }
             const newNode = new Node(value);
+            newNode.parent = this;
             this.left = newNode;
             return;
         }
+    }
 
+    find (value) {
+        if (this.value === value) {
+            return this;
+        }
 
+        if (this.value < value && this.right) {
+            return this.right.find(value);
+        }
+
+        if (this.value > value && this.left) {
+            return this.left.find(value);
+        }
+    }
+
+    remove(value) {
+        const identifiedNode = this.find(value);
+        if (!identifiedNode) {
+            throw new Error('not here');
+        }
+        if (!identifiedNode.left && !identifiedNode.right) {
+            const identifiedParent = identifiedNode.parent;
+            identifiedParent.removeChild(identifiedNode);
+        }
+    }
+
+    removeChild(node) {
+        if (this.left && this.left === node) {
+            this.left = null;
+            return;
+        }
+        if (this.right && this.right === node) {
+            this.right = null;
+        }
     }
 }
 
@@ -45,3 +90,15 @@ class Tree {
         this.root.find(value)
     }
 }
+
+const newTree = new Tree();
+
+newTree.add(5)
+newTree.add(1)
+newTree.add(7)
+newTree.add(32)
+newTree.add(77)
+newTree.add(10)
+
+newTree.find(5)
+console.log(newTree)
